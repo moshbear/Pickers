e99fa6d572d6628cb69fb8235ba9afa155f87c9f
andrey <andrey@moshbear.net>
2021-05-11 04:58:02 -0400

    DateTimeUtils: refactor and introduce updateDt*
    updateDtDate, updateDtTime are helpers to update the date and time in a container-agnostic format (assuming it's still in a (Mutable)LiveData)

--- app/src/main/java/com/moshy/pickersdemo/DateTimeUiUtils.kt
+++ app/src/main/java/com/moshy/pickersdemo/DateTimeUiUtils.kt
@@ -1,15 +1,83 @@
+/*
+* Copyright 2021 Andrey V
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.moshy.pickersdemo
 
 import android.content.Context
+import android.text.format.DateFormat
 import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
-// This block of code is re-used extensively in other ViewModel's, but because it is UI-bound,
-// it does not belong in DateTimeUtils.kt
+internal data class DateFormatConfiguration(val locale: Locale, val is24Hour: Boolean)
+// Singleton to cache SimpleDateFormat for given DateFormatConfiguration
+private val DateFormatSettings = object {
+    private lateinit var dateFormat: DateFormatConfiguration
+
+    private lateinit var bestDateTimePattern: String
+
+    private lateinit var formatter: SimpleDateFormat
+
+    fun getSimpleDateFormatter(df: DateFormatConfiguration): SimpleDateFormat {
+        if ((!::dateFormat.isInitialized)
+            || (dateFormat != df))
+        {
+            dateFormat = df
+
+            bestDateTimePattern = DateFormat.getBestDateTimePattern(dateFormat.locale,
+                /* skeleton */ when (dateFormat.is24Hour) {
+                    true -> "y-M-d\nHms"
+                    false -> "y-M-d\nhms"
+                })
+
+            formatter = SimpleDateFormat(bestDateTimePattern, dateFormat.locale)
+        }
+        return formatter
+    }
+}
+
+internal fun timestampToDateTimeString(df: DateFormatConfiguration, time: Long): String =
+    synchronized(DateFormatSettings) {
+        DateFormatSettings.getSimpleDateFormatter(df).format(Date(time * 1000))
+    }
+
 internal fun dtStringView(dt: LiveData<Calendar>, df: DateFormatConfiguration,
                           context: Context, @StringRes resId: Int) =
     Transformations.map(dt) {
         context.getString(resId, timestampToDateTimeString(df, it.timeInMillis / 1000))
     }
+
+internal fun updateDtDate(dt: MutableLiveData<Calendar>, y: Int, m: Int, d: Int) =
+    dt.value?.run {
+        val c = this.clone() as Calendar // force copy
+        c[Calendar.YEAR] = y
+        c[Calendar.MONTH] = m
+        c[Calendar.DAY_OF_MONTH] = d
+        dt.value = c
+    } ?: check(false)
+
+internal fun updateDtTime(dt: MutableLiveData<Calendar>, h: Int, m: Int, s: Int) =
+    dt.value?.run {
+        val c = this.clone() as Calendar // force copy
+        c[Calendar.HOUR_OF_DAY] = h
+        c[Calendar.MINUTE] = m
+        c[Calendar.SECOND] = s
+        dt.value = c
+    } ?: check(false)
--- app/src/main/java/com/moshy/pickersdemo/DateTimeUtils.kt
+++ app/src/main/java/com/moshy/pickersdemo/DateTimeUtils.kt
@@ -15,43 +15,7 @@
 */
 package com.moshy.pickersdemo
 
-import android.text.format.DateFormat
-import java.text.SimpleDateFormat
 import java.util.Calendar
-import java.util.Date
-import java.util.Locale
-
-internal data class DateFormatConfiguration(val locale: Locale, val is24Hour: Boolean)
-// Singleton to cache SimpleDateFormat for given DateFormatConfiguration
-private val DateFormatSettings = object {
-    private lateinit var dateFormat: DateFormatConfiguration
-
-    private lateinit var bestDateTimePattern: String
-
-    private lateinit var formatter: SimpleDateFormat
-
-    fun getSimpleDateFormatter(df: DateFormatConfiguration): SimpleDateFormat {
-        if ((!::dateFormat.isInitialized)
-            || (dateFormat != df))
-        {
-            dateFormat = df
-
-            bestDateTimePattern = DateFormat.getBestDateTimePattern(dateFormat.locale,
-                /* skeleton */ when (dateFormat.is24Hour) {
-                    true -> "y-M-d\nHms"
-                    false -> "y-M-d\nhms"
-                })
-
-            formatter = SimpleDateFormat(bestDateTimePattern, dateFormat.locale)
-        }
-        return formatter
-    }
-}
-
-internal fun timestampToDateTimeString(df: DateFormatConfiguration, time: Long): String =
-    synchronized(DateFormatSettings) {
-        DateFormatSettings.getSimpleDateFormatter(df).format(Date(time * 1000))
-    }
 
 internal fun timestampToCalendar(ts: Long): Calendar =
     Calendar.getInstance().apply {
